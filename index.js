1f9a77eedadc4a7feb7c357ec96e9c928251b5c0
Cloud Stone <cloudstone.me@gmail.com>
2016-03-31 19:08:18 +0800

    Hello World

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var path = require('path');
+var program = require('commander') // Parse command line options
+var pkg = require( path.join(__dirname, 'package.json') );
+
+
+program
+	.version(pkg.version)
+	.option('-f, --force', 'Create new file whatever the file is exists.')
+	.parse(process.argv);
+
+var force = program.force || false;
+
+function createFile(dir) {
+	var file = dir+'/index.php';
+
+	fs.exists(file, (exists) => {
+		if(exists && !force) {
+			console.log('Exists: '+dir+'/index.php');
+		} else {
+			fs.writeFile(file, "<?php\n//Silence is golden!", (err) => {
+				if (err) throw err;
+				console.log('Created: '+dir+'/index.php');
+			});
+		}
+	});
+}
+
+function walk(dir) {
+	createFile(dir);
+
+	fs.readdirSync(dir).filter(function (f) {
+		return f && f[0] != '.'; // Ignore hidden files
+	})
+	.map(function (f) {
+		var subdir = (dir + '/' + f).replace('./', ''),
+		stat = fs.statSync(subdir);
+
+		if (stat.isDirectory()) {
+			createFile(subdir);
+			walk(subdir);
+		}
+	});
+}
+
+walk('.');
